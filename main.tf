3f2b656bf20eae7b21f56edba490214be582d693
nangelpt <76477210+nangelpt@users.noreply.github.com>
2023-07-05 21:32:54 +0100

    corrected

--- main.tf
+++ main.tf
@@ -32,15 +32,15 @@ vpc_security_group_ids = [module.blog.sg.security_group_id]
 module "blog_sg" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "5.1.0"
-  name    = "blog_new
+  name    = "blog_new"
   
   vpc_id              = data.aws_vpc.default.id
 
   ingress_rules       = ["http-80-tcp","https-443-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
 
   egress_rules       = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0]
+  egress_cidr_blocks = ["0.0.0.0/0"]
 }
 
 resource "aws_security_group" "blog" {
